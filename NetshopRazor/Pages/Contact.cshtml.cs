95d184fd4683b3cdd3462a284385f738c312226c
init6restart <jagoanlangitku@gmail.com>
2026-01-11 19:00:40 +0700

    Add project files.

---
+++ NetshopRazor/Pages/Contact.cshtml.cs
@@ -0,0 +1,19 @@
+
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace NetshopRazor.Pages
+{
+    public class ContactModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+
+        public void OnPost()
+        {
+
+        }
+
+	}
+}
