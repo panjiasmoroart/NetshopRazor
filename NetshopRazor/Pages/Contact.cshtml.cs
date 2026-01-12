60c2d4755ee673161fbdf88d9f94a4552dee9b1e
Panji Asmoro <pandjiasmoroart@gmail.com>
2026-01-12 21:44:21 +0700

    Contact Form - Traditional Validation

--- NetshopRazor/Pages/Contact.cshtml.cs
+++ NetshopRazor/Pages/Contact.cshtml.cs
@@ -1,4 +1,7 @@
 
+using System.ComponentModel.DataAnnotations;
+using System.Numerics;
+using System.Security.Cryptography.X509Certificates;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
@@ -10,10 +13,47 @@ namespace NetshopRazor.Pages
         {
         }
 
+        // contact form - traditional validation
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Phone { get; set; } = "";
+        public string Subject { get; set; } = "";
+        public string Message { get; set; } = "";
+
+        public string SuccessMessage { get; set; } = "";
+        public string ErrorMessage { get; set; } = "";
+
         public void OnPost()
         {
+            FirstName = Request.Form["FirstName"];
+            LastName = Request.Form["LastName"];
+            Email = Request.Form["Email"];
+            Phone = Request.Form["Phone"];
+            Subject = Request.Form["Subject"];
+            Message = Request.Form["Message"];
 
-        }
+            // check if any required field is empty
+            if (FirstName.Length == 0 || LastName.Length == 0 || Email.Length == 0  || Subject.Length == 0 || Message.Length == 0)
+            {
+                ErrorMessage = "Please fill all required fields";
+                return;
+            }
 
+            // Add this message to the database 
+
+            // Send Confirmation Email to the client 
+
+            SuccessMessage = "Your message has been received correctly";
+
+
+
+			FirstName = "";
+			LastName = "";
+			Email = "";
+			Phone = "";
+			Subject = "";
+			Message = "";
+		}
 	}
 }
