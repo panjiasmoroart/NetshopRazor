a6c85c1525f9d0cacb780e9c8382215e7ba4e697
Panji Asmoro <pandjiasmoroart@gmail.com>
2026-01-17 16:14:59 +0700

    Messages - Add Table Pagination

--- NetshopRazor/Pages/Admin/Messages/Index.cshtml.cs
+++ NetshopRazor/Pages/Admin/Messages/Index.cshtml.cs
@@ -4,25 +4,54 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace NetshopRazor.Pages.Admin.Messages
 {
-    public class IndexModel : PageModel
-    {
+	public class IndexModel : PageModel
+	{
 		public List<MessageInfo> listMessages = new List<MessageInfo>();
+		public int page = 1; // the current html page 
+		public int totalPages = 0; // total pages
+		private readonly int pageSize = 2; // each html page shows pageSize messages
+
 		public void OnGet()
-        {
-			try 
+		{
+			page = 1;
+			string requestPage = Request.Query["page"];
+			if (requestPage != null)
 			{
-				string connectionString = "Data Source=.\\sqlexpress;Initial Catalog=netshoprazor_db;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
+				try
+				{
+					page = int.Parse(requestPage);
+				}
+				catch (Exception ex)
+				{
+					page = 1;
+				}
+			}
+			string connectionString = "Data Source=.\\sqlexpress;Initial Catalog=netshoprazor_db;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
+
+			try
+			{
 				using (SqlConnection connection = new SqlConnection(connectionString))
 				{
 					connection.Open();
 
-					string sqlCount = "SELECT * FROM messages ORDER BY id DESC";
+					string sqlCount = "SELECT COUNT(*) FROM messages";
 					using (SqlCommand command = new SqlCommand(sqlCount, connection))
 					{
+						decimal count = (int)command.ExecuteScalar();
+						totalPages = (int)Math.Ceiling(count / pageSize);
+					}
+
+					string sql = "SELECT * FROM messages ORDER BY id DESC";
+					sql += " OFFSET @skip ROWS FETCH NEXT @pageSize ROWS ONLY";
+
+					using (SqlCommand command = new SqlCommand(sql, connection))
+					{
+						command.Parameters.AddWithValue("@skip", (page - 1) * pageSize);
+						command.Parameters.AddWithValue("@pageSize", pageSize);
+
 						using (SqlDataReader reader = command.ExecuteReader())
 						{
 							while (reader.Read())
-
 							{
 								MessageInfo messageInfo = new MessageInfo();
 								messageInfo.Id = reader.GetInt32(0);
@@ -39,26 +68,24 @@ namespace NetshopRazor.Pages.Admin.Messages
 						}
 					}
 				}
-
-
-			} 
-			catch (Exception ex) 
+			}
+			catch (Exception ex)
 			{
 				Console.WriteLine(ex.Message);
 			}
-
-        }
-
-		public class MessageInfo
-		{
-			public int Id { get; set; }
-			public string FirstName { get; set; } = "";
-			public string LastName { get; set; } = "";
-			public string Email { get; set; } = "";
-			public string Phone { get; set; } = "";
-			public string Subject { get; set; } = "";
-			public string Message { get; set; } = "";
-			public string CreatedAt { get; set; } = "";
 		}
 	}
+
+	public class MessageInfo
+	{
+		public int Id { get; set; }
+		public string FirstName { get; set; } = "";
+		public string LastName { get; set; } = "";
+		public string Email { get; set; } = "";
+		public string Phone { get; set; } = "";
+		public string Subject { get; set; } = "";
+		public string Message { get; set; } = "";
+		public string CreatedAt { get; set; } = "";
+	}
+	
 }
