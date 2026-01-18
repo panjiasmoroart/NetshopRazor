f50970783d30794f4dbf6514bc0c312065f2de39
Panji Asmoro <pandjiasmoroart@gmail.com>
2026-01-18 23:52:31 +0700

    Authentication & Authorization - Login Create Authenticated Session

--- NetshopRazor/Pages/Auth/Login.cshtml.cs
+++ NetshopRazor/Pages/Auth/Login.cshtml.cs
@@ -1,4 +1,7 @@
 using System.ComponentModel.DataAnnotations;
+using System.Data.SqlClient;
+using System.Data.SqlClient;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Filters;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -42,6 +45,65 @@ namespace NetshopRazor.Pages.Auth
 			// successfull data validation
 
 			// connect to database and check the user credentials
+			try
+			{
+				string connectionString = "Data Source=.\\sqlexpress;Initial Catalog=netshoprazor_db;Integrated Security=True;Encrypt=True;TrustServerCertificate=True";
+				using (SqlConnection connection = new SqlConnection(connectionString))
+				{
+					connection.Open();
+					string sql = "SELECT * FROM users WHERE email=@email";
+
+					using (SqlCommand command = new SqlCommand(sql, connection))
+					{
+						command.Parameters.AddWithValue("@email", Email);
+						using (SqlDataReader reader = command.ExecuteReader())
+						{
+							if (reader.Read())
+							{
+								int id = reader.GetInt32(0);
+								string firstname = reader.GetString(1);
+								string lastname = reader.GetString(2);
+								string email = reader.GetString(3);
+								string phone = reader.GetString(4);
+								string address = reader.GetString(5);
+								string hashedPassword = reader.GetString(6);
+								string role = reader.GetString(7);
+								string created_at = reader.GetDateTime(8).ToString("MM/dd/yyyy");
+
+								// verify the password
+								var passwordHasher = new PasswordHasher<IdentityUser>();
+								var result = passwordHasher.VerifyHashedPassword(new IdentityUser(),
+									hashedPassword, Password);
+
+								if (result == PasswordVerificationResult.Success || result == PasswordVerificationResult.SuccessRehashNeeded)
+								{
+									// successful password verification => initialize the session
+									HttpContext.Session.SetInt32("id", id);
+									HttpContext.Session.SetString("firstname", firstname);
+									HttpContext.Session.SetString("lastname", lastname);
+									HttpContext.Session.SetString("email", email);
+									HttpContext.Session.SetString("phone", phone);
+									HttpContext.Session.SetString("address", address);
+									HttpContext.Session.SetString("role", role);
+									HttpContext.Session.SetString("created_at", created_at);
+
+									// the user is authenticated successfully => redirect to the home page
+									Response.Redirect("/");
+								}
+							}
+						}
+
+					}
+
+				}
+			}
+			catch (Exception ex)
+			{
+				errorMessage = ex.Message;
+				return;
+			}
+
+
 			// Wrong Email or Password
 			errorMessage = "Wrong Email or Password";
 		}
