79e02f367d99b240ac975fc9743db6b806d5e477
Panji Asmoro <pandjiasmoroart@gmail.com>
2026-01-20 16:54:02 +0700

    Create Orders - Home Page (Read Book Items From Database)

--- NetshopRazor/Pages/Index.cshtml.cs
+++ NetshopRazor/Pages/Index.cshtml.cs
@@ -1,13 +1,90 @@
+using System.Data.SqlClient;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NetshopRazor.Pages.Admin.Books;
 
 namespace NetshopRazor.Pages
 {
-    public class IndexModel : PageModel
+	public class IndexModel : PageModel
     {
-        public void OnGet()
-        {
+		public List<BookInfo> listNewestBooks = new List<BookInfo>();
+		public List<BookInfo> listTopSales = new List<BookInfo>();
 
-        }
-    }
-}
+		private readonly string connectionString;
+
+		public IndexModel(IConfiguration configuration)
+		{
+			connectionString = configuration.GetConnectionString("DefaultConnection")!;
+		}
+
+		public void OnGet()
+		{
+			try
+			{
+				using (SqlConnection connection = new SqlConnection(connectionString))
+				{
+					connection.Open();
+
+					string sql = "SELECT TOP 4 * FROM books ORDER BY created_at DESC";
+					using (SqlCommand command = new SqlCommand(sql, connection))
+					{
+						using (SqlDataReader reader = command.ExecuteReader())
+						{
+							while (reader.Read())
+							{
+								BookInfo bookInfo = new BookInfo();
+								bookInfo.Id = reader.GetInt32(0);
+								bookInfo.Title = reader.GetString(1);
+								bookInfo.Authors = reader.GetString(2);
+								bookInfo.Isbn = reader.GetString(3);
+								bookInfo.NumPages = reader.GetInt32(4);
+								bookInfo.Price = reader.GetDecimal(5);
+								bookInfo.Category = reader.GetString(6);
+								bookInfo.Description = reader.GetString(7);
+								bookInfo.ImageFileName = reader.GetString(8);
+								bookInfo.CreatedAt = reader.GetDateTime(9).ToString("MM/dd/yyyy");
+
+								listNewestBooks.Add(bookInfo);
+							}
+						}
+					}
+
+					sql = "SELECT TOP 4 books.*, (" +
+					   "SELECT SUM(order_items.quantity) FROM order_items WHERE books.id = order_items.book_id" +
+					   ") AS total_sales " +
+					   "FROM books " +
+					   "ORDER BY total_sales DESC";
+
+					using (SqlCommand command = new SqlCommand(sql, connection))
+					{
+						using (SqlDataReader reader = command.ExecuteReader())
+						{
+							while (reader.Read())
+							{
+								BookInfo bookInfo = new BookInfo();
+								bookInfo.Id = reader.GetInt32(0);
+								bookInfo.Title = reader.GetString(1);
+								bookInfo.Authors = reader.GetString(2);
+								bookInfo.Isbn = reader.GetString(3);
+								bookInfo.NumPages = reader.GetInt32(4);
+								bookInfo.Price = reader.GetDecimal(5);
+								bookInfo.Category = reader.GetString(6);
+								bookInfo.Description = reader.GetString(7);
+								bookInfo.ImageFileName = reader.GetString(8);
+								bookInfo.CreatedAt = reader.GetDateTime(9).ToString("MM/dd/yyyy");
+
+								listTopSales.Add(bookInfo);
+							}
+						}
+					}
+				}
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+
+		}
+
+	}
+	}
