5a7f66c1405c56aa7bc7a504a19fb2a524c7d4ce
Panji Asmoro <pandjiasmoroart@gmail.com>
2026-01-21 17:25:45 +0700

    Create Orders - Cart Details [Convert Cookie to Dictionary]

--- NetshopRazor/Pages/Cart.cshtml.cs
+++ NetshopRazor/Pages/Cart.cshtml.cs
@@ -7,12 +7,51 @@ namespace NetshopRazor.Pages
     public class CartModel : PageModel
     {
 		public List<OrderItem> listOrderItems = new List<OrderItem>();
+
+		private readonly string connectionString;
+		public CartModel(IConfiguration configuration)
+		{
+			connectionString = configuration.GetConnectionString("DefaultConnection")!;
+		}
+
+		private Dictionary<String, int> getBookDictionary()
+		{
+			var bookDictionary = new Dictionary<string, int>();
+
+			// Read shopping cart items from cookie
+			string cookieValue = Request.Cookies["shopping_cart"] ?? "";
+
+			if (cookieValue.Length > 0)
+			{
+				string[] bookIdArray = cookieValue.Split('-');
+
+				for (int i = 0; i < bookIdArray.Length; i++)
+				{
+					string bookId = bookIdArray[i];
+					if (bookDictionary.ContainsKey(bookId))
+					{
+						bookDictionary[bookId] += 1;
+					}
+					else
+					{
+						bookDictionary.Add(bookId, 1);
+					}
+				}
+			}
+
+			return bookDictionary;
+		}
+
 		public void OnGet()
         {
-        }
-    }
+			// Read shopping cart items from cookie
+			var bookDictionary = getBookDictionary();
+			Console.WriteLine("Book dictionary count: " + bookDictionary.Count);
+		}
+	}
 
-    public class OrderItem
+
+	public class OrderItem
     {
 		public BookInfo bookInfo = new BookInfo();
 		public int numCopies = 0;
