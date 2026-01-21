cd6ed681f45b4871512e3534c33a37368c1e2a3b
Panji Asmoro <pandjiasmoroart@gmail.com>
2026-01-21 15:38:17 +0700

    Create Orders - Cart Details [Create OrderItem Class]

--- NetshopRazor/Pages/Cart.cshtml.cs
+++ NetshopRazor/Pages/Cart.cshtml.cs
@@ -1,12 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using NetshopRazor.Pages.Admin.Books;
 
 namespace NetshopRazor.Pages
 {
     public class CartModel : PageModel
     {
-        public void OnGet()
+		public List<OrderItem> listOrderItems = new List<OrderItem>();
+		public void OnGet()
         {
         }
     }
+
+    public class OrderItem
+    {
+		public BookInfo bookInfo = new BookInfo();
+		public int numCopies = 0;
+		public decimal totalPrice = 0;
+	}
 }
