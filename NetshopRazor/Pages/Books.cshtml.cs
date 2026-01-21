698b1bf1efa0596756bc72d5c85bb58d224ae6c8
Panji Asmoro <pandjiasmoroart@gmail.com>
2026-01-21 15:26:07 +0700

    Create Orders - Book Search [Implement Pagination]

--- NetshopRazor/Pages/Books.cshtml.cs
+++ NetshopRazor/Pages/Books.cshtml.cs
@@ -16,6 +16,10 @@ namespace NetshopRazor.Pages
 
 		public List<BookInfo> listBooks = new List<BookInfo>();
 
+		public int page = 1; // the current html page
+		public int totalPages = 0;
+		private readonly int pageSize = 5; // books per page
+
 		private readonly string connectionString;
 		public BooksModel(IConfiguration configuration)
 		{
@@ -24,11 +28,68 @@ namespace NetshopRazor.Pages
 
 		public void OnGet()
         {
+			page = 1;
+			string requestPage = Request.Query["page"];
+			if (requestPage != null)
+			{
+				try
+				{
+					page = int.Parse(requestPage);
+				}
+				catch (Exception ex)
+				{
+					page = 1;
+				}
+			}
+
 			try
 			{
 				using (SqlConnection connection = new SqlConnection(connectionString))
 				{
 					connection.Open();
+
+					string sqlCount = "SELECT COUNT(*) FROM books";
+					sqlCount += " WHERE (title LIKE @search OR authors LIKE @search)";
+
+					if (PriceRange.Equals("0_50"))
+					{
+						sqlCount += " AND price <= 50";
+					}
+					else if (PriceRange.Equals("50_100"))
+					{
+						sqlCount += " AND price >= 50 AND price <= 100";
+					}
+					else if (PriceRange.Equals("above100"))
+					{
+						sqlCount += " AND price >= 100";
+					}
+
+					if (PageRange.Equals("0_100"))
+					{
+						sqlCount += " AND num_pages <= 100";
+					}
+					else if (PageRange.Equals("100_299"))
+					{
+						sqlCount += " AND num_pages >= 100 AND num_pages <= 299";
+					}
+					else if (PageRange.Equals("above300"))
+					{
+						sqlCount += " AND num_pages >= 300";
+					}
+
+					if (!Category.Equals("any"))
+					{
+						sqlCount += " AND category=@category";
+					}
+
+					using (SqlCommand command = new SqlCommand(sqlCount, connection))
+					{
+						command.Parameters.AddWithValue("@search", "%" + Search + "%");
+						command.Parameters.AddWithValue("@category", Category);
+
+						decimal count = (int)command.ExecuteScalar();
+						totalPages = (int)Math.Ceiling(count / pageSize);
+					}
 
 					string sql = "SELECT * FROM books";
 					sql += " WHERE (title LIKE @search OR authors LIKE @search)";
@@ -64,10 +125,15 @@ namespace NetshopRazor.Pages
 						sql += " AND category=@category";
 					}
 
+					sql += " ORDER BY id DESC";
+					sql += " OFFSET @skip ROWS FETCH NEXT @pageSize ROWS ONLY";
+
 					using (SqlCommand command = new SqlCommand(sql, connection))
 					{
 						command.Parameters.AddWithValue("@search", "%" + Search + "%");
 						command.Parameters.AddWithValue("@category", Category);
+						command.Parameters.AddWithValue("@skip", (page - 1) * pageSize);
+						command.Parameters.AddWithValue("@pageSize", pageSize);
 
 						using (SqlDataReader reader = command.ExecuteReader())
 						{
