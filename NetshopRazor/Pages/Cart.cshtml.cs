a969d5ebd1cdb3b05bd0668c2686355dcfeb7dfd
Panji Asmoro <pandjiasmoroart@gmail.com>
2026-01-21 18:00:47 +0700

    Create Orders - Cart Details [List Cart Items]

--- NetshopRazor/Pages/Cart.cshtml.cs
+++ NetshopRazor/Pages/Cart.cshtml.cs
@@ -1,3 +1,4 @@
+using System.Data.SqlClient;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using NetshopRazor.Pages.Admin.Books;
@@ -46,7 +47,58 @@ namespace NetshopRazor.Pages
         {
 			// Read shopping cart items from cookie
 			var bookDictionary = getBookDictionary();
-			Console.WriteLine("Book dictionary count: " + bookDictionary.Count);
+			//Console.WriteLine("Book dictionary count: " + bookDictionary.Count);
+
+			// action can be null, "add", "sub" or "delete"
+			string? action = Request.Query["action"];
+			string? id = Request.Query["id"];
+
+			try
+			{
+				using (SqlConnection connection = new SqlConnection(connectionString))
+				{
+					connection.Open();
+
+					string sql = "SELECT * FROM books WHERE id=@id";
+					foreach (var keyValuePair in bookDictionary)
+					{
+						string bookID = keyValuePair.Key;
+						using (SqlCommand command = new SqlCommand(sql, connection))
+						{
+							command.Parameters.AddWithValue("@id", bookID);
+
+							using (SqlDataReader reader = command.ExecuteReader())
+							{
+								if(reader.Read())
+								{
+									OrderItem item = new OrderItem();
+
+									item.bookInfo.Id = reader.GetInt32(0);
+									item.bookInfo.Title = reader.GetString(1);
+									item.bookInfo.Authors = reader.GetString(2);
+									item.bookInfo.Isbn = reader.GetString(3);
+									item.bookInfo.NumPages = reader.GetInt32(4);
+									item.bookInfo.Price = reader.GetDecimal(5);
+									item.bookInfo.Category = reader.GetString(6);
+									item.bookInfo.Description = reader.GetString(7);
+									item.bookInfo.ImageFileName = reader.GetString(8);
+									item.bookInfo.CreatedAt = reader.GetDateTime(9).ToString("MM/dd/yyyy");
+
+									item.numCopies = keyValuePair.Value;
+									item.totalPrice = item.numCopies * item.bookInfo.Price;
+
+									listOrderItems.Add(item);
+								}
+							}
+						}
+					}
+				}
+
+			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
 		}
 	}
 
